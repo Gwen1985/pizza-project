de849f5b4fc2b49990f05a58c6a830446aff3c8e
gwen1985 <gwen.ellegeest@outlook.com>
2020-10-27 10:50:47 +0100

    initial setup mvc structure

--- Views/page.php
+++ Views/page.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta content="karolonka&copy; & G.WebDev&copy;" name="author">
+    <meta content="karolonka&copy; & G.WebDev&copy;" name="authors">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title> Pineapple Pizzeria </title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
@@ -10,6 +10,7 @@
 </head>
 
 <body>
+
 <?php require 'header.php'; ?>
 
 <?php require 'main.php'; ?>
