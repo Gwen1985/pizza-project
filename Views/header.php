6f44aee8fba069f38841e8477d05967b96599881
gwen1985 <gwen.ellegeest@outlook.com>
2020-10-30 00:47:17 +0100

    setting up front-end part header.php

--- Views/header.php
+++ Views/header.php
@@ -1,5 +1,4 @@
 <header>
-
     <div class="row pt-2 pb-2" id="upperRow">
 
         <div class="col-md-9 ">
@@ -7,7 +6,8 @@
 
         <div class="col-md-3">
             <a href="mailto:gwen.ellegeest@outlook.com">
-                <svg width="20" height="20" viewBox="0 0 16 16" class="bi bi-envelope text-light" fill="currentColor"
+                <svg width="18" height="18" viewBox="0 0 16 16" class="bi bi-envelope text-light ml-4"
+                     fill="currentColor"
                      xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd"
                           d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2zm13 2.383l-4.758 2.855L15 11.114v-5.73zm-.034 6.878L9.271 8.82 8 9.583 6.728 8.82l-5.694 3.44A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.739zM1 11.114l4.758-2.876L1 5.383v5.73z"/>
@@ -15,7 +15,7 @@
             </a>
 
             <a>
-                <svg width="20" height="20" viewBox="0 0 16 16" class="bi bi-chat-dots text-light ml-3"
+                <svg width="18" height="18" viewBox="0 0 16 16" class="bi bi-chat-dots text-light ml-4"
                      fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd"
                           d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"/>
@@ -27,9 +27,60 @@
     </div>
 
     <div class="content_area">
-        <!--bg image in here -->
-        <h1>Pineapple Pizzeria</h1>
+
+        <div class="row">
+
+            <div class="col-md-7">
+            </div>
+
+            <div class="content col-md-5 pt-5">
+                <a href="homePage.php" class="text-light"> Home </a>
+                <a href="#" class="text-light ml-5"> About </a>
+                <a href="#" class="text-light ml-5"> Menu </a>
+                <a href="#" class="text-light ml-5"> Contact </a>
+            </div>
+
+        </div>
+
+        <div class="row d-flex justify-content-center text-light">
+
+            <div class="content col-md-2 d-flex justify-content-center">
+            </div>
+
+            <div class="content col-md-8 d-flex justify-content-center">
+                <h1>Pineapple Pizzeria</h1>
+            </div>
+
+            <div class="content col-md-2 d-flex justify-content-center">
+            </div>
+
+        </div>
+
+        <div class="row  text-light">
+
+            <div class="content col-md-2 ">
+            </div>
+
+            <div class="content col-md-8 " id="bestPizza">
+                <h2>The Best Pizzeria In Belgium</h2>
+                <br>
+                <button class="btn btn-warning " type="button"><a href="bookingPage.php" class="text-light"> Book a table</a></button>
+            </div>
+
+            <div class="content col-md-2 ">
+            </div>
+
+        </div>
+
+
     </div>
+
+
+    <!---->
+    <!--    <div class="content_area">-->
+    <!--        bg image in here -->
+    <!--        <h1>Pineapple Pizzeria</h1>-->
+    <!--    </div>-->
 
 
 </header>
