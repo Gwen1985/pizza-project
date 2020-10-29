5c43b3b2aa5483f8f6d35f6aaabbd4410380f8da
gwen1985 <gwen.ellegeest@outlook.com>
2020-10-30 00:48:03 +0100

    testing booking form on bookingPage.php

--- Views/bookingPage.php
+++ Views/bookingPage.php
@@ -1,1 +1,5 @@
-<?php
+<div> <?php require 'Views/bookTableViewForm.php'; ?>
+    <?php require 'Controllers/bookTableController.php'; ?>
+    <?php require 'Models/BookTablePoster.php'; ?>
+
+</div>
--- Views/main.php
+++ Views/main.php
@@ -9,6 +9,3 @@
 
 
 
-<!--    --><?php //require 'Views/bookTableViewForm.php'; ?>
-<!--    --><?php //require 'Controllers/bookTableController.php'; ?>
-<!--    --><?php //require 'Models/BookTablePoster.php'; ?>
